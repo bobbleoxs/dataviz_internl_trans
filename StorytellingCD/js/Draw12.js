0341021346312d9d8bde644071b03c52c5550f76
bobbleoxs <shanxsun@gmail.com>
2018-10-09 19:49:10 +0100

    Update Draw12.js
    update wording

--- StorytellingCD/js/Draw12.js
+++ StorytellingCD/js/Draw12.js
@@ -9,10 +9,10 @@ function Draw12(){
 	runProgressBar(time=700*11);
 
 	changeTopText(newText = "I want to note here that the UK has all chords connected to it " +
-							" but the colour of them are all grey, i.e. UK's colour",
+							" but the colour of them are all red, i.e. UK's colour",
 		loc = 3/2, delayDisappear = 0, delayAppear = 1, finalText = false, xloc=-80, w=210);
 	changeTopText(newText = "It means that the UK has always had the net income. " +
-							"Or the UK always received more than it paid out for IC transactions",
+							"Or the UK always received more than it paid out for international transactions",
 		loc = 3/2, delayDisappear = 9, delayAppear = 10, finalText = true, xloc=-80, w=210);
 
 };/*Draw12*/
