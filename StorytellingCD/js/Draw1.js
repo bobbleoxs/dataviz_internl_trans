07471a49b939ba78b169c0d66dc5afd3a0e5cc4a
bobbleoxs <shanxsun@gmail.com>
2018-10-09 19:59:22 +0100

    Update Draw1.js

--- StorytellingCD/js/Draw1.js
+++ StorytellingCD/js/Draw1.js
@@ -7,21 +7,21 @@ function Draw1(){
 	stopClicker();
 
 	/*Show and run the progressBar*/
-	runProgressBar(time=700*11);
+	runProgressBar(time=500*11);
 
 	changeTopText(newText = "International transactions within a multinational enterprise can be difficult to track with endless rows of data." +
 							" Chord diagram is a technique which could help turn abstract data into stunning visualisaiton. ",
 	loc = 4/2, delayDisappear = 0, delayAppear = 1);
 
 	changeTopText(newText = "Next I will introduce using the chord diagram to represent international transactions. ",
-	loc = 8/2, delayDisappear = 7, delayAppear = 8, finalText = true);
+	loc = 8/2, delayDisappear = 5, delayAppear = 6, finalText = true);
 
 	changeBottomText(newText = "The network IC transactions are based on simulated data for illustration.",
-	loc = 1/2, delayDisappear = 0, delayAppear = 8);
+	loc = 1/2, delayDisappear = 0, delayAppear = 7);
 
 	//Remove arcs again
 	d3.selectAll(".arc")
-		.transition().delay(9*700).duration(2000)
+		.transition().delay(9*500).duration(1000)
 		.style("opacity", 0)
 		.each("end", function() {d3.selectAll(".arc").remove();});
 
