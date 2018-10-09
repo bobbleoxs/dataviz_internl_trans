1ed3b47c815b6fe61ab2f831d86d620733a86ae1
bobbleoxs <shanxsun@gmail.com>
2018-10-09 19:30:52 +0100

    Update Draw1.js
    update general wording

--- StorytellingCD/js/Draw1.js
+++ StorytellingCD/js/Draw1.js
@@ -9,11 +9,11 @@ function Draw1(){
 	/*Show and run the progressBar*/
 	runProgressBar(time=700*11);
 
-	changeTopText(newText = "Internal cross border transactions can be difficult to track with endless rows of data." +
-							" Chord diagram is a stunning data visualisaiton technique with a wide range of applications.",
+	changeTopText(newText = "International transactions within a multinational enterprise can be difficult to track with endless rows of data." +
+							" Chord diagram is a technique which could help turn abstract data into stunning visualisaiton."",
 	loc = 4/2, delayDisappear = 0, delayAppear = 1);
 
-	changeTopText(newText = "Next I will introduce using the chord diagram to represent internal cross-border transactions within Omnicom. ",
+	changeTopText(newText = "Next I will introduce using the chord diagram to represent international transactions. ",
 	loc = 8/2, delayDisappear = 7, delayAppear = 8, finalText = true);
 
 	changeBottomText(newText = "The network IC transactions are based on the data provided to me for 2017.",
