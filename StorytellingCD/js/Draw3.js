a69e06c3f5b0a318fc5b5d889412d600c9d896bb
bobbleoxs <shanxsun@gmail.com>
2018-10-09 19:52:36 +0100

    Update Draw3.js
    change timing

--- StorytellingCD/js/Draw3.js
+++ StorytellingCD/js/Draw3.js
@@ -6,7 +6,7 @@ function Draw3(){
 	/*First disable click event on clicker button*/
 	stopClicker();
 
-	var arcDelay = [0,1,2,12,13,23,33,34,35,40,47];
+	var arcDelay = [0,1,2,10,12,15,25,30,32,37,42];
 	/*Show and run the progressBar*/
 	runProgressBar(time=700*(arcDelay[(arcDelay.length-1)]+1));
 
@@ -48,13 +48,13 @@ function Draw3(){
 		loc = 3/2, delayDisappear = (arcDelay[5]-1), delayAppear = arcDelay[5]);
 	/*France*/
 	changeTopText(newText = "France has slightly more than 4%",
-		loc = 4/2, delayDisappear = arcDelay[6], delayAppear = (arcDelay[8]-1));
+		loc = 4/2, delayDisappear = arcDelay[6], delayAppear = (arcDelay[7]-1));
 	/*100%*/
 	changeTopText(newText = "Together that sums up to 100%",
-		loc = 1/2, delayDisappear = (arcDelay[9]-1), delayAppear = arcDelay[9]);
+		loc = 1/2, delayDisappear = (arcDelay[8]-1), delayAppear = arcDelay[8]);
 	/*Chord intro*/
 	changeTopText(newText = "This circle shows how the IC transactions are currently divided between the countries",
-		loc = 8/2, delayDisappear = (arcDelay[10]-1), delayAppear = arcDelay[10], finalText = true);
+		loc = 8/2, delayDisappear = (arcDelay[9]-1), delayAppear = arcDelay[9], finalText = true);
 
 	/*Change the text of the bottom section inside the circle accordingly*/
 	/*Canada*/
@@ -65,9 +65,9 @@ function Draw3(){
 		loc = -1/2, delayDisappear = arcDelay[3], delayAppear = arcDelay[4]);
 	/*Mexico*/
 	changeBottomText(newText = "Finally the Netherlands has around 10%.",
-		loc = -1/2, delayDisappear = (arcDelay[5]-1), delayAppear = (arcDelay[8]-1));
+		loc = -1/2, delayDisappear = (arcDelay[5]-1), delayAppear = (arcDelay[7]-1));
 	/*Chord intro*/
 	changeBottomText(newText = "Now we're going to look at how IC transactions flowed from one country to another",
-		loc = 1/2, delayDisappear = (arcDelay[9]-1), delayAppear = arcDelay[10]);
+		loc = 1/2, delayDisappear = (arcDelay[8]-1), delayAppear = arcDelay[8]);
 
 };/*Draw3*/
